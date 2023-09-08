085d9f80e3c4d7312f1c40e4a5e1af92aac1ea8c
Cory <standardvr@gmail.com>
2023-09-08 09:37:42 -0500

    Finished exercises and first studio

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student newStudent = new Student("Cory", 7618, 1, 4.0);
+        Teacher newTeacher = new Teacher("Gary", "Oak", "Battles", 25);
+
+        System.out.println("**********\nNew Student\n**********\nName: " + newStudent.getName() + "\nID: " + newStudent.getStudentId() + "\nCredits: " + newStudent.getNumberOfCredits() + "\nGPA: " + newStudent.getGpa() + "\n**********");
+        System.out.println("New Teacher\n**********\n" + newTeacher.getFirstName() + " " + newTeacher.getLastName() + "\nSubject: " + newTeacher.getSubject() + "\nYears Teaching: " + newTeacher.getYearsTeaching());
     }
 }
